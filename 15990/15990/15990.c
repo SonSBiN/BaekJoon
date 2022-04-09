53fd78b3152ad445c0a27f14683281215cb8a672
SonSBiN <victor990914@naver.com>
2022-04-09 23:41:12 +0900

    second commit

--- 15990/15990/15990.c
+++ 15990/15990/15990.c
@@ -1,10 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<time.h>
 
-int n;
 
-int dfs() {
+int count;
+int sub[] = {1,2,3};
 
+void dfs(int a,int n) {
+
+	n -= a;
+	for (int i = 0; i < 3; i++)
+	{
+		if (n - sub[i] >= 0 && a != sub[i])
+		{
+			if (n - sub[i] == 0) {
+				count++;
+				return;
+			}
+			dfs(sub[i],n);
+		}
+	}
 }
 
 
@@ -12,11 +27,23 @@ int dfs() {
 int main(void)
 {
 	int t;
+	int n;
 	scanf("%d", &t);
-
+	/*
+	3
+	4
+	7
+	10
+	*/
 	for (int i = 0; i < t; i++)
 	{
+		count = 0;
 		scanf("%d", &n);
+		for (int j = 0; j < 3; j++)
+		{
+			dfs(sub[j],n);
+		}
+		printf("%d\n", count);
 	}
 
 	return 0;
