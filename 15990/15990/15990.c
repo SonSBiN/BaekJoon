4c0876108cf494dbcf9ff17c05de32dfbdf9960b
SonSBiN <victor990914@naver.com>
2022-04-09 22:18:51 +0900

    first commit

---
+++ 15990/15990/15990.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int n;
+
+int dfs() {
+
+}
+
+
+
+int main(void)
+{
+	int t;
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d", &n);
+	}
+
+	return 0;
+}
