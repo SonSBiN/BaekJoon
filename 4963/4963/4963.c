dabd9dfd51807f7db81087077214cf0e2c5c1f76
SonSBiN <victor990914@naver.com>
2022-04-16 20:20:03 +0900

    Success

--- 4963/4963/4963.c
+++ 4963/4963/4963.c
@@ -1,64 +1,95 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int count;
-int visit[51][51] = {0,};
-int map[51][51] = {0,};
-int w, h;
-int dx[] = { -1,-1,-1,0,0,1,1,1 };
-int dy[] = { -1,0,1,-1,1,-1,0,1 };
+int map[50][50];
+int visited[50][50];
+int land_cnt;
 
-void Count_i(int y, int x) {
-	for (int i = 0; i < 8; i++) {
-		if (x + dx[i] > 0 && x + dx[i] <= w && y + dy[i] > 0 && y + dy[i] <= h) {
-			if (visit[y + dy[i]][x + dx[i]] == 0) {
-				visit[y + dy[i]][x + dx[i]] = 1;
-				if (map[y + dy[i]][x + dx[i]] == 1) {
-					Count_i(y + dy[i], x + dx[i]);
-				}
-			}
-		}
-	}
+int x[8] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int y[8] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+struct Node {
+    int x;
+    int y;
+    struct Node* next;
+
+};
+
+void bfs(struct Node* head, int w, int h) {
+    struct Node* temp, * new, * use;
+    int X, Y;
+
+    use = head->next;
+
+    while (head->next != NULL) {
+        temp = head->next;
+
+        for (int i = 0; i < 8; i++) {
+            X = temp->x + x[i];
+            Y = temp->y + y[i];
+
+            if (X < 0 || X >= h || Y < 0 || Y >= w) {
+                continue;
+            }
+            if (visited[X][Y] == 1 || map[X][Y] == 0) {
+                continue;
+            }
+
+            visited[X][Y] = 1;
+            new = (struct Node*)malloc(sizeof(struct Node));
+            new->x = X;
+            new->y = Y;
+            new->next = NULL;
+            use->next = new;
+            use = use->next;
+        }
+
+        head->next = temp->next;
+        free(temp);
+    }
+    land_cnt++;
 }
-
-void Find_i() {
-	for (int i = 1; i <= h; i++) {
-		for (int j = 1; j <= w; j++) {
-			if (map[i][j] == 1 && visit[i][j] == 0) {
-				visit[i][j] = 1;
-				count++;
-				Count_i(i, j);
-			}
-		}
-	}
-	return;
-}
-
 
 
 int main(void) {
+    int w, h;
+    struct Node* head, * new;
 
+    while (1)
+    {
+        scanf("%d %d", &w, &h);
 
-	do{
-		count = 0;
-		scanf("%d %d", &w, &h);
-		if (w != 0 || h != 0) {
-			for (int i = 1; i <= w; i++) {
-				for (int j = 1; j <= h; j++) {
-					scanf("%d", &map[i][j]);
-				}
-			}
-			Find_i();
-			printf("count = %d\n", count);
-			for (int i = 1; i <= w; i++) {
-				for (int j = 1; j <= h; j++) {
-					map[i][j] = 0;
-					visit[i][j] = 0;
-				}
-			}
-		}
-		
-	} while (w != 0 || h != 0);
-	return 0;
+        if (w == 0 && h == 0)
+            break;
+
+        land_cnt = 0;
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                scanf("%d", &map[i][j]);
+                visited[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (visited[i][j] == 1 || map[i][j] == 0)
+                    continue;
+                visited[i][j] = 1;
+                head = (struct Node*)malloc(sizeof(struct Node));
+                new = (struct Node*)malloc(sizeof(struct Node));
+                new->x = i;
+                new->y = j;
+                new->next = NULL;
+                head->next = new;
+
+                bfs(head, w, h);
+            }
+        }
+
+        printf("%d\n", land_cnt);
+
+    }
 }
+
